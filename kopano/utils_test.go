b27681dd0c88a50b97621ee9228493a6e98d77cf
Philipp Holzer <admin@philipp.info>
2018-11-21 21:10:20 +0100

    some test refactoring

--- kopano/utils_test.go
+++ kopano/utils_test.go
@@ -1,9 +1,12 @@
 package kopano
 
 import (
+	"errors"
+	"fmt"
 	"github.com/magiconair/properties/assert"
 	"github.com/nupplaphil/kopano-ldap/mocks"
 	"gopkg.in/ldap.v2"
+	"strconv"
 	"testing"
 )
 
@@ -11,70 +14,118 @@ func TestLdapBoolToStr(t *testing.T) {
 	assertTrue := "yes"
 	assertFalse := "no"
 
-	test := LdapBoolToStr("1")
-	assert.Equal(t, test, assertTrue)
+	tests := map[string]struct {
+		value     string
+		assertVal string
+	}{
+		"test1": {"1", assertTrue},
+		"test2": {"0", assertFalse},
+		"test3": {"", assertFalse},
+		"test4": {"10", assertTrue},
+		"test5": {"test", assertTrue},
+	}
 
-	test = LdapBoolToStr("0")
-	assert.Equal(t, test, assertFalse)
-
-	test = LdapBoolToStr("")
-	assert.Equal(t, test, assertFalse)
-
-	test = LdapBoolToStr("10")
-	assert.Equal(t, test, assertTrue)
-
-	test = LdapBoolToStr("test")
-	assert.Equal(t, test, assertTrue)
+	for _, test := range tests {
+		testEx := LdapBoolToStr(test.value)
+		assert.Equal(t, testEx, test.assertVal)
+	}
 }
 
 func TestLdapArrayToStr(t *testing.T) {
-	assertStr1 := "test1, test2, test3"
-	str1 := LdapArrayToStr([]string{"test1", "test2", "test3"}, ",")
-	assert.Equal(t, str1, assertStr1)
+	tests := map[string]struct {
+		values    []string
+		separator string
+		assertVal string
+	}{
+		"test1": {
+			[]string{"test1", "test2", "test3"},
+			",",
+			"test1, test2, test3",
+		},
+		"test2": {
+			[]string{"test1", "test2", "test3"},
+			".",
+			"test1. test2. test3",
+		},
+		"test3": {
+			[]string{"test1", "test2", "test3"},
+			"",
+			"test1 test2 test3",
+		},
+	}
 
-	assertStr2 := "test1. test2. test3"
-	str2 := LdapArrayToStr([]string{"test1", "test2", "test3"}, ".")
-	assert.Equal(t, str2, assertStr2)
-
-	assertStr3 := "test1 test2 test3"
-	str3 := LdapArrayToStr([]string{"test1", "test2", "test3"}, "")
-	assert.Equal(t, str3, assertStr3)
+	for _, test := range tests {
+		testStr := LdapArrayToStr(test.values, test.separator)
+		assert.Equal(t, testStr, test.assertVal)
+	}
 }
 
 func TestGetBaseDN(t *testing.T) {
-	assertBaseDn1 := "dc=example,dc=org"
-	baseDn1 := GetBaseDN("example.org")
-	assert.Equal(t, baseDn1, assertBaseDn1)
+	tests := map[string]struct {
+		domain string
+		baseDn string
+	}{
+		"test1": {"example.org", "dc=example,dc=org"},
+		"test2": {"", "<nil>"},
+		"test3": {"1.2.", "dc=1,dc=2"},
+	}
 
-	assertBaseDn2 := "<nil>"
-	baseDn2 := GetBaseDN("")
-	assert.Equal(t, baseDn2, assertBaseDn2)
-
-	assertBaseDn3 := "dc=1,dc=2"
-	baseDn3 := GetBaseDN("1.2.")
-	assert.Equal(t, baseDn3, assertBaseDn3)
+	for _, test := range tests {
+		testDn := GetBaseDN(test.domain)
+		assert.Equal(t, testDn, test.baseDn)
+	}
 }
 
 func TestGetNextIDs(t *testing.T) {
 	tests := map[string]struct {
-		baseDn string
-		uid    string
-		gid    string
+		baseDn    string
+		uid       string
+		gid       string
+		searchErr error
+		error     error
 	}{
-		"test1": {
+		"testNormal": {
 			"example.org",
 			"1",
-			"1",
+			"2",
+			nil,
+			nil,
 		},
-		"test2": {
+		"testTest0": {
 			"example.org",
 			"0",
-			"0",
+			"1",
+			nil,
+			nil,
 		},
-		"test3": {
+		"testNegSearch": {
 			"example.",
 			"-1",
-			"-1",
+			"-2",
+			fmt.Errorf("an expected error"),
+			fmt.Errorf("an expected error"),
+		},
+		"testNegCastUid": {
+			"example.",
+			"bla",
+			"-3",
+			nil,
+			&strconv.NumError{
+				Func: "Atoi",
+				Num:  "bla",
+				Err:  errors.New("invalid syntax"),
+			},
+		},
+		"testNegCastGid": {
+			"example.",
+			"-4",
+			"blub",
+			nil,
+			&strconv.NumError{
+				Func: "Atoi",
+				Num:  "blub",
+				Err:  errors.New("invalid syntax"),
+			},
 		},
 	}
 
@@ -102,9 +153,17 @@ func TestGetNextIDs(t *testing.T) {
 		}
 
 		client := &mocks.Client{}
-		client.On("Search", &searchRequest).Return(&searchResult, nil).Once()
+		client.On("Search", &searchRequest).Return(&searchResult, test.searchErr).Once()
 		client.On("Close").Once()
 
-		GetNextIDs(client, test.baseDn)
+		uid, gid, err := GetNextIDs(client, test.baseDn)
+		assert.Equal(t, err, test.error)
+
+		if err == nil {
+			assertUid, _ := strconv.Atoi(test.uid)
+			assert.Equal(t, uid, assertUid+1)
+			assertGid, _ := strconv.Atoi(test.gid)
+			assert.Equal(t, gid, assertGid+1)
+		}
 	}
 }
