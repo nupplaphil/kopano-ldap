650556f913cc18cbe04130f626f7e738ff4d1a07
Philipp Holzer <admin@philipp.info>
2018-11-24 22:43:03 +0100

    test fixings

--- test.sh
+++ test.sh
@@ -4,7 +4,6 @@ set -e
 echo "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
-    go test -race -coverprofile=profile.out -covermode=atomic $d
     go test -race -coverporfile=profile.out -covermode=atomic -tags integration $d
     if [[ -f profile.out ]]; then
         cat profile.out >> coverage.txt
